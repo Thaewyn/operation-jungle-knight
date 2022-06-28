89e89d252ce9c83492b74be2ab04f11ec4af734e
Thaewyn <thaewyn@gmail.com>
2022-06-28 19:23:42 -0400

    added stubbed gamecontroller tests

--- __test__/controllers/game_controller.test.js
+++ __test__/controllers/game_controller.test.js
@@ -1,5 +1,38 @@
 const GameController = require('../../controllers/game_controller.js');
 
-test('creates a GameController object', () => {
-  const gameController = new GameController();
-});
+describe("GameController", () => {
+  it("should construct a new GameController instance when initialized", () => {
+
+    const gameController = new GameController();
+    expect(gameController).toBeInstanceOf(GameController);
+  });
+
+  describe("handleCombat", () => {
+
+    it("should have a handleCombat function", () => {
+
+    });
+
+    it("should accept userid, runid, and turn_data params", () => {
+
+    })
+
+    it("should error if userid is not provided", () => {
+
+    })
+
+    it("should error if runid is not provided", () => {
+
+    })
+
+    it("should return a 'result' object with valid data", () => {
+
+    })
+  });
+
+  describe("validateTurnSubmission", () => {
+
+
+    
+  })
+})
