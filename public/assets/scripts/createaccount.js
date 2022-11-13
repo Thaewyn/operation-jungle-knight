e6ddb55dd58e7c7d320e730575c394f809a643fe
jaquel <jaquelrey@gmail.com>
2022-11-13 17:30:06 -0500

    add: modal popup on account creation error; redirect from creation page to main game menu on success

--- public/assets/scripts/createaccount.js
+++ public/assets/scripts/createaccount.js
@@ -1,4 +1,23 @@
 // console.log("loaded createaccount.js")
+function displayModal(message) {
+  document.querySelector(".lightbox").style.display = "flex";
+  if (message) {
+    document.querySelector(".modal > .msg").textContent = message;
+  }
+}
+
+document.querySelector("#go_home_button")?.addEventListener("click", (e) => {
+  window.location.href = "/";
+});
+
+document.querySelector(".closebutton > a")?.addEventListener("click", (e) => {
+  e.preventDefault();
+  hideModal();
+});
+
+function hideModal() {
+  document.querySelector(".lightbox").style.display = "none";
+}
 
 document.querySelector("#createform").addEventListener("submit", (e) => {
   e.preventDefault();
@@ -6,37 +25,41 @@ document.querySelector("#createform").addEventListener("submit", (e) => {
   let data = {
     user: "",
     email: "",
-    pass: ""
-  }
-  
+    pass: "",
+  };
+
   for (let i = 0; i < e.target.children.length; i++) {
     let el = e.target.children[i];
     if (el.type == "text") {
-      data.user = el.value
+      data.user = el.value;
     } else if (el.type == "email") {
-      data.email = el.value
+      data.email = el.value;
     } else if (el.type == "password") {
-      data.pass = el.value
+      data.pass = el.value;
     }
   }
 
-  let str = JSON.stringify(data)
+  let str = JSON.stringify(data);
 
   if (data.user && data.pass) {
-    fetch("/api/user/create",{
+    fetch("/api/user/create", {
       method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: str
+      body: str,
     })
-    .then((res) => res.json())
-    .then((data) => {
-      // console.log("success")
-      // console.log(data);
-    })
-    .catch((err) => {
-      // console.error("Error: "+err);
-    })
+      .then((res) => {
+        res.json();
+        if (res.status === 200) {
+          window.location.href = "/menu";
+        } else {
+          displayModal("Account creation failed!");
+          document.querySelector("input[name='password']").value = "";
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
-})
+});
